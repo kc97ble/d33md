d1fe3d1d9033ac37e4166365077d59498dac948d
Kien Nguyen <kc97ble@gmail.com>
2020-06-21 22:31:04 +0800

    7

---
+++ backend/src/latexize.ts
@@ -0,0 +1,204 @@
+import { text } from "body-parser";
+
+type Tree = {
+  type: string;
+  children: string | Array<Tree>;
+};
+
+function sanitize(s: string): string {
+  return s;
+}
+
+function getRole(text: string) {
+  let h = 0;
+  let result = "";
+  let c = 65;
+  for (let i = 0; i < text.length; i++) {
+    if (/\(\[\{/.test(text[i])) {
+      h += 1;
+    }
+    result += h == 0 ? "." : String.fromCharCode(c);
+    if (/\)\]\}/.test(text[i])) {
+      h -= 1;
+      c += 1;
+    }
+  }
+  return result;
+}
+
+function rule1(text: string): Tree {
+  const role = getRole(text);
+  for (let i = 0; i < text.length; i++) {
+    if (!/[A-Z]/.test(role[i])) {
+      if (!/[a-z0-9A-Z_^+\-*\/<=>! ]/.test(text[i])) {
+        return {
+          type: "concat",
+          children: [
+            rule2(text.substring(0, i)),
+            { type: "literal", children: text[i] },
+            rule1(text.substring(i + 1)),
+          ],
+        };
+      }
+    }
+  }
+  return rule2(text);
+}
+
+function rule2(text: string): Tree {
+  const role = getRole(text);
+  for (let i = 0; i < text.length; i++) {
+    if (!/[A-Z]/.test(role[i])) {
+      if (/[<>=!]/.test(text[i])) {
+        let j = i;
+        while (/[<>=!]/.test(text[j])) j += 1;
+        return {
+          type: "relation",
+          children: [
+            rule3(text.substring(0, i)),
+            { type: "literal", children: text.substring(i, j) },
+            rule2(text.substring(j)),
+          ],
+        };
+      }
+    }
+  }
+  return rule3(text);
+}
+
+function rule3(text: string): Tree {
+  const role = getRole(text);
+  for (let i = 0; i < text.length; i++) {
+    if (!/[A-Z]/.test(role[i])) {
+      if (/[+\-*\/]/.test(text[i])) {
+        let j = i + 1;
+        return {
+          type: "arithmetic",
+          children: [
+            rule4(text.substring(0, i)),
+            { type: "literal", children: text.substring(i, j) },
+            rule3(text.substring(j)),
+          ],
+        };
+      }
+    }
+  }
+  return rule4(text);
+}
+
+function rule4(text: string): Tree {
+  const role = getRole(text);
+  for (let i = 0; i < text.length; i++) {
+    if (!/[A-Z]/.test(role[i])) {
+      if (/[_^]/.test(text[i])) {
+        let j = i + 1;
+        return {
+          type: "script",
+          children: [
+            rule5(text.substring(0, i)),
+            { type: "literal", children: text.substring(i, j) },
+            rule4(text.substring(j)),
+          ],
+        };
+      }
+    }
+  }
+  return rule5(text);
+}
+
+function rule5(text: string): Tree {
+  const role = getRole(text);
+  for (let i = 0; i + 1 < text.length; i++) {
+    if (role[i] != role[i + 1]) {
+      return {
+        type: "concat",
+        children: [
+          rule6(text.substring(0, i + 1)),
+          rule5(text.substring(i + 1)),
+        ],
+      };
+    }
+  }
+  return rule6(text);
+}
+
+function rule6(text) {
+  if (text === "") return { type: "literal", children: "" };
+  if (text[0] === "(" && text[text.length - 1] === ")") {
+    return {
+      type: "concat",
+      children: [
+        { type: "literal", children: "(" },
+        rule1(text.substring(1, text.length - 1)),
+        { type: "literal", children: ")" },
+      ],
+    };
+  }
+  if (text[0] === "[" && text[text.length - 1] === "]") {
+    return {
+      type: "concat",
+      children: [
+        { type: "literal", children: "[" },
+        rule1(text.substring(1, text.length - 1)),
+        { type: "literal", children: "]" },
+      ],
+    };
+  }
+  if (text[0] === "{" && text[text.length - 1] === "}") {
+    return {
+      type: "concat",
+      children: [rule1(text.substring(1, text.length - 1))],
+    };
+  }
+  return {
+    type: "literal",
+    children: text,
+  };
+}
+
+function opToLatex(text: string): string {
+  const MAP = {
+    "<=": "\\le",
+    ">=": "\\ge",
+    "==": "=",
+    "!=": "\\ne",
+    "===": "\\equiv",
+    "*": "\\times",
+  };
+  return sanitize(MAP[text] || text);
+}
+
+function treeToLatex(node: Tree): string {
+  if (node.type === "literal") {
+    return sanitize(opToLatex(node.children as string));
+  } else if (node.type === "concat") {
+    return (node.children as Array<Tree>)
+      .map((child) => treeToLatex(child))
+      .join(" ");
+  } else if (node.type === "relation") {
+    return [
+      treeToLatex((node.children as Array<Tree>)[0]),
+      treeToLatex((node.children as Array<Tree>)[1]),
+      treeToLatex((node.children as Array<Tree>)[2]),
+    ].join(" ");
+  } else if (node.type === "arithmetic") {
+    return [
+      treeToLatex((node.children as Array<Tree>)[0]),
+      treeToLatex((node.children as Array<Tree>)[1]),
+      treeToLatex((node.children as Array<Tree>)[2]),
+    ].join(" ");
+  } else if (node.type === "script") {
+    return [
+      "{" + treeToLatex((node.children as Array<Tree>)[0]) + "}",
+      treeToLatex((node.children as Array<Tree>)[1]),
+      "{" + treeToLatex((node.children as Array<Tree>)[2]) + "}",
+    ].join(" ");
+  } else {
+    throw Error("invalid");
+  }
+}
+
+export default function latexize(text: string): string {
+  const node = rule1(text);
+  return treeToLatex(node);
+}
--- backend/src/logic.ts
+++ backend/src/logic.ts
@@ -1,18 +1,143 @@
 import marked from "marked";
 
+function sanitize(text: string) {
+  return text;
+}
+
 class CustomRenderer extends marked.Renderer {
-  heading = (text, level) => {
-    return text;
+  code(
+    code: string,
+    language: string | undefined,
+    isEscaped: boolean
+  ): string {
+    return "?? :D ??";
+  }
+  blockquote(quote: string): string {
+    return "?? :D ??";
+  }
+  html(html: string): string {
+    return "?? :D ??";
+  }
+  heading = (
+    text: string,
+    level: 1 | 2 | 3 | 4 | 5 | 6,
+    raw: string,
+    slugger: marked.Slugger
+  ): string => {
+    if (level === 1) {
+      return `\\problemtitle{${sanitize(text)}}\n`;
+    } else if (level === 2) {
+      return `\\heading{${sanitize(text)}}\n`;
+    } else {
+      return "?? :D ??";
+    }
   };
+  // hr(): string {
+  //   return "?? :D ??";
+  // }
+  // list(body: string, ordered: boolean, start: number): string {
+  //   return "?? :D ??";
+  // }
+  // listitem(text: string): string {
+  //   return "?? :D ??";
+  // }
+  // checkbox(checked: boolean): string {
+  //   return "?? :D ??";
+  // }
+  paragraph(text: string): string {
+    return sanitize(text);
+  }
+  // table(header: string, body: string): string {
+  //   return "?? :D ??";
+  // }
+  // tablerow(content: string): string {
+  //   return "?? :D ??";
+  // }
+  // tablecell(
+  //   content: string,
+  //   flags: {
+  //     header: boolean;
+  //     align: "center" | "left" | "right" | null;
+  //   }
+  // ): string {
+  //   return "?? :D ??";
+  // }
+  // strong(text: string): string {
+  //   return "?? :D ??";
+  // }
+  // em(text: string): string {
+  //   return "?? :D ??";
+  // }
+  codespan(code: string): string {
+    return `\\texttt{${sanitize(code)}}`;
+  }
+  // br(): string {
+  //   return "?? :D ??";
+  // }
+  // del(text: string): string {
+  //   return "?? :D ??";
+  // }
+  // link(
+  //   href: string | null,
+  //   title: string | null,
+  //   text: string
+  // ): string {
+  //   return "?? :D ??";
+  // }
+  // image(
+  //   href: string | null,
+  //   title: string | null,
+  //   text: string
+  // ): string {
+  //   return "?? :D ??";
+  // }
+  // text(text: string): string {
+  //   return "?? :D ??";
+  // }
 }
 
 const renderer = new CustomRenderer();
 
-// @ts-ignore
-marked.use({ renderer: renderer });
+const walker = (token) => {
+  console.log(token.raw);
+  console.log(token.text);
+};
 
 export function mdToLatex(text: string) {
-  return marked(text);
+  const tokens = marked.lexer(text);
+  (marked as any).walkTokens(tokens, walker);
+  const result = marked.parser(tokens, {
+    renderer: new CustomRenderer(),
+  });
+  return result;
 }
 
-console.log(mdToLatex("# Heading"));
+console.log(
+  mdToLatex(`
+  # ACCEPTED
+  Tí và Sửu mới tập code. Vì vậy, code để biên dịch được đã khó, code để bài nộp đạt yêu cầu còn khó hơn. Hôm nay, thầy Dần cho Tí và Sửu \`N\` bài tập. Bài tập thứ \`i\` có giá trị điểm bằng \`a[i]\`. Điểm số của mỗi người sẽ bằng tổng giá trị điểm của các bài tập mà người đó làm được. Vì không muốn bị phạt, Tí và Sửu tìm đến Mão nhờ sự trợ giúp.
+  
+  Mão đặt vào một chiếc hộp đen \`N\` lá thăm, ghi các số từ \`1\` đến \`N\` và không có hai lá thăm nào ghi cùng số. Tí và Sửu sẽ lần lượt bốc ngẫu nhiên một lá thăm trong chiếc hộp đen. Sau khi bốc được một lá thăm ghi số \`X\`, Mão sẽ code cho người rút được lá thăm này bài tập \`X\`. Tí và Sửu sẽ thay phiên nhau bốc các lá thăm cho đến khi chiếc hộp đen không còn lá thăm nào.
+  Hãy tìm chênh lệch điểm tối đa giữa Tí và Sửu.
+  
+  ## Dữ liệu
+  
+  - Dòng đầu tiên chứa số nguyên \`N\` , là số lượng bài tập \`(1 <= N <= 50)\`.
+  - Dòng thứ hai chứa \`N\` số nguyên \`a[1], a[2], ..., a[N] (1 <= a[i] <= 50)\`, là điểm số của các bài tập.
+  
+  ## Kết quả
+  
+  In ra chênh lệch điểm tối đa giữa Tí và Sửu.
+  
+  ## Ví dụ
+  
+  \`\`\`
+  3
+  1 2 3
+  \`\`\`
+  \`\`\`
+  4
+  \`\`\`
+  
+`)
+);
